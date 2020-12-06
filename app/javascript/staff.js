98be11f6b9beae7b01deb471ba1a51dc9dadcf1f
cochimu <cochimu1221@icloud.com>
2020-12-06 10:37:29 +0900

    jQuery導入前

---
+++ app/javascript/staff.js
@@ -0,0 +1,6 @@
+function add() {
+  const addbutton = document.getElementById("add-button");
+  addbutton.addEventListener("click",(e) => {});
+  console.log("Hello")
+}
+window.addEventListener("load", add);
